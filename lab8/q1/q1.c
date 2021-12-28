15d381f81522d6bfeb5f7a95a301e37a978a9df3
haimbacharr <81071510+haimbacharr@users.noreply.github.com>
2021-12-28 12:10:20 +0200

    Add files via upload

--- lab8/q1/q1.c
+++ lab8/q1/q1.c
@@ -1,21 +1,88 @@
 #include "head.h"
 
-void main()
+void Push(PStack s, int   new_elem)
 {
-	int i, flag, del_value, arr[NUM] = { 1,4,3,2 };
-	Stack s;
-	s.head = NULL;
-	s.size = 0;
-	for (i = 0; i < NUM; i++)
+	int flag = 1, i = 1;
+	Item* temp, * run;
+	if (s->size == NUM)
 	{
-		Push(&s, arr[i]);
+		fprintf(stdout, "The stack is full!!\n");
+		flag = 0;
 	}
-	while (s.size)
-	{
-		flag = Pop(&s, &del_value);
-		if (flag)
-			fprintf(stdout, "\n%d was deleted", del_value);
+	if (flag) {
+		run = s->head;
+		temp = (Item*)malloc(sizeof(struct Item));
+		if (temp == NULL)//If allocation failed.
+		{
+			DeleteList(s);
+			Error_Msg("Memmory!");
+		}
+		temp->num = new_elem;
+		temp->next = NULL;
+		if (NULL == s->head)//if its the first time to insert a block.
+		{
+			s->head = temp;
+			run = s->head;
+			s->size++;
+			fprintf(stdout, "%d was inserted\n", s->head->num);
+		}
+		else //if its not the first block in the list.
+		{
+			while (i < s->size)   //Makes the run pointer to point at the one block before the last one in the list.
+			{
+				i++;
+				run = run->next;
+			}
+			run->next = temp;
+			run = temp;
+			s->size++;
+			fprintf(stdout, "%d was inserted\n", run->num);
+			
+			if (s->size == NUM) fprintf(stdout, "The stack is full!!\n");
+		}
 	}
-	fprintf(stdout, "\nThe stack is empty.");
+}
 
+void DeleteList(PStack s)
+{
+	Item* temp;
+	while (s->head)
+	{
+		temp = s->head;
+		s->head = s->head->next;
+		free(temp);
+	}
+	s->size = 0;
+}
+int Pop(PStack s, int* del_value)
+{
+	Item* run;
+	int i = 1;
+	run = s->head;
+	while (i < s->size - 1)
+	{
+		run = run->next;
+		i++;
+	}
+	if (NULL!=run->next) {
+		*del_value = run->next->num;
+		free(run->next); //free the last block in the list.
+		run->next = NULL;
+		s->size--;
+		return 1;
+	}
+	else        //if its the last block in the list.
+	{
+		*del_value = run->num;
+		free(run);
+		s->size--;
+		s = NULL;
+		return 1;
+	}
+	return 0;
+}
+void Error_Msg(char* str)
+{
+	fprintf(stderr, "%s\n",str);
+	exit(1);
 }
